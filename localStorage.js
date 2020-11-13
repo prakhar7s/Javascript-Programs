e3f890051ea178f5efc46b78335013354a432d8e
prakhar7s <56044776+prakhar7s@users.noreply.github.com>
2020-11-13 23:56:40 +0530

    local storage

---
+++ localStorage.js
@@ -0,0 +1,11 @@
+//save item in local storage
+localStorage.setItem("name", "prakhar");
+
+// //fetch item from local storage
+console.log(localStorage.getItem("name"));
+
+//delete item from local storage
+localStorage.removeItem("name");
+
+//delete all item from local storage
+localStorage.clear();
