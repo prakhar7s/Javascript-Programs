b7a307332133b8be12d5325d533d6e6504be85d3
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2021-11-28 17:30:06 +0530

    firebase firestore and location related codes

---
+++ firebase-javascript/script.js
@@ -0,0 +1,14 @@
+// db.collection("apartments").onSnapshot((d) => {
+//   d.docs.map((d) => {
+//     db.collection("city")
+//       .doc("mUYWZuKZD09ZGo4flvCm")
+//       .collection("locality")
+//       .doc("a9OhS5nl3TZSVsUpSXaF")
+//       .collection("catalogue")
+//       .doc()
+//       .set({
+//         ...d.data(),
+//       })
+//       .then((d) => {});
+//   });
+// });
---
+++ user-location/script.js
@@ -0,0 +1,7 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(console.log);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
