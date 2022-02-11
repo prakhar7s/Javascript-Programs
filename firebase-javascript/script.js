57eba250180757752e7a9c8203e57955401cb6ec
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-02-11 16:21:36 +0530

    firebase javscript code

--- firebase-javascript/script.js
+++ firebase-javascript/script.js
@@ -12,3 +12,34 @@
 //       .then((d) => {});
 //   });
 // });
+
+db.collection("city")
+  .where("name", "==", "Bangalore")
+  .get()
+  .then((d) => {
+    if (d.docs.length) {
+      // city doc already exists
+
+      d.docs[0].ref
+        .collection("locality")
+        .where("name", "==", "Whitefield")
+        .get()
+        .then((dd) => {
+          if (dd.docs.length) {
+            dd.docs[0].ref
+              .collection("catalogue")
+              .doc("newnewnewnew")
+              .set({
+                sdsdd: "sdsda",
+              })
+              .then((ddd) => {
+                console.log("done");
+              });
+          } else {
+            // create new docs for its locality
+          }
+        });
+    } else {
+      // create new docs for its city
+    }
+  });
